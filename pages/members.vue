83632caee45c99e6939d80f6e9b7a6dd8b3670a0
iceStorm <hauduadaiduong@gmail.com>
2021-08-18 14:20:03 +0700

    re-style members page

--- pages/members.vue
+++ pages/members.vue
@@ -1,15 +1,52 @@
+<i18n>
+{
+  "vi": {
+    "members": {
+      "boardsTitle": "Các thành viên BQT",
+      "starsTitle": "Các thành viên nổi bật"
+    }
+  },
+  "en": {
+    "members": {
+      "boardsTitle": "Board Members",
+      "starsTitle": "Stars"
+    }
+  }
+}
+</i18n>
+
 <template>
   <div class="members-page">
 
-    <div class="members-page-category" v-for="(role, categoryKey) in getBoardMembers" :key=categoryKey>
-      <h3 class="title members-page-category-title">{{ role.roleName }}</h3>
+    <!-- thành viên trong Ban Quản trị -->
+    <div class="members-page-boards">
+      <h1 class="title">{{ $t('members.boardsTitle') }}</h1>
 
-      <div class="members-page-category-members">
-        <div class="members-page-category-members-item" v-for="member in role.members" :key="member.id">
-          <img :src="member.meta.avatar">
-          <h3 class="text">{{ member.title.rendered }}</h3>
+      <div class="members-page-boards-members">
+        <div class="members-page-boards-members-head">
+          <!-- Đội Trưởng/Đội Phó -->
+          <div class="members-page-boards-members-item" v-for="member in getBoardHeadMembers" :key="member.id">
+            <img :src="member.meta.avatar">
+            <h3 class="text">{{ member.meta.role }}</h3>
+            <h3 class="text">{{ member.title.rendered }}</h3>
+          </div>
+        </div>
+
+        <div class="members-page-boards-members-others">
+          <!-- Không phải Đội Trưởng/Đội Phó -->
+          <div class="members-page-boards-members-item" v-for="member in getBoardOtherMembers" :key="member.id">
+            <img :src="member.meta.avatar">
+            <h3 class="text">{{ member.meta.role }}</h3>
+            <h3 class="text">{{ member.title.rendered }}</h3>
+          </div>
         </div>
       </div>
+    </div>
+
+    <!-- thành viên nổi bật -->
+    <div class="members-page-stars" v-if="false">
+      <h1 class="title">{{ $t('members.starsTitle') }}</h1>
+      <!--  -->
     </div>
 
   </div>
@@ -33,23 +70,23 @@ export default {
   },
 
   computed: {
-    getBoardMembers() {
-      // getting roles
-      const roles = this.getMemberRoles(this.boardMembers);
-      const roleIds = Object.keys(roles).map((role) => parseInt(role, 10));
+    getBoardHeadMembers() {
+      let boardHeadMembers = Object.values(this.getBoardMembers())
+        .filter((role) => role.roleId === 32 || role.roleId === 35 || role.roleId === 36 || role.roleId === 37)
+        .map((role) => role.members);
 
-      this.boardMembers.forEach((member) => {
-        if (roleIds.includes(member.member_roles[0])) {
-          if (!roles[member.member_roles[0]].members) {
-            roles[member.member_roles[0]].members = [];
-          }
+      boardHeadMembers = [].concat.apply([], boardHeadMembers);
+      // console.log(boardHeadMembers);
 
-          roles[member.member_roles[0]].members.push(member);
-        }
-      });
+      return boardHeadMembers;
+    },
+    getBoardOtherMembers() {
+      let boardOtherMembers = Object.values(this.getBoardMembers())
+        .filter((role) => role.roleId !== 32 && role.roleId !== 35 && role.roleId !== 36 && role.roleId !== 37)
+        .map((role) => role.members);
 
-      // console.log('roles:', roles);
-      return roles;
+      boardOtherMembers = [].concat.apply([], boardOtherMembers);
+      return boardOtherMembers;
     },
   },
 
@@ -75,6 +112,24 @@ export default {
 
       return roles;
     },
+    getBoardMembers() {
+      // getting roles
+      const roles = this.getMemberRoles(this.boardMembers);
+      const roleIds = Object.keys(roles).map((role) => parseInt(role, 10));
+
+      this.boardMembers.forEach((member) => {
+        if (roleIds.includes(member.member_roles[0])) {
+          if (!roles[member.member_roles[0]].members) {
+            roles[member.member_roles[0]].members = [];
+          }
+
+          roles[member.member_roles[0]].members.push(member);
+        }
+      });
+
+      console.log('roles:', roles);
+      return roles;
+    },
   },
 };
 </script>
@@ -83,45 +138,118 @@ export default {
 .members-page {
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 50px;
 
-  &-category {
-    &-title {
-      text-transform: uppercase;
-
-      padding-bottom: 5px;
-      margin-bottom: 15px;
-
-      border-bottom: 1px solid rgba(154, 154, 154, 0.26);
+  &-boards {
+    .title {
+      padding-bottom: 20px;
+      text-align: center;
     }
 
     &-members {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-      gap: 10px;
+      display: flex;
+      flex-direction: column;
+      gap: 35px;
 
       &-item {
         display: flex;
         flex-direction: column;
-        gap: 5px;
-
         width: 200px;
-        background: rgb(248, 248, 248);
-        box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+
+        img {
+          width: 150px;
+          height: 150px;
+
+          margin: auto;
+          margin-bottom: 10px;
+
+          object-fit: cover;
+          border-radius: 50%;
+          box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+        }
 
         h3 {
           text-align: center;
           font-size: 13px;
-        }
+          margin-bottom: 0;
 
-        img {
-          width: 100%;
-          height: 200px;
-          object-fit: cover;
-          border-bottom: 1px solid rgba(154, 154, 154, 0.5);
+          &:first-child {
+            // font-size: 9px !important;
+          }
+
+          &:last-child {
+            font-weight: bolder;
+          }
         }
       }
+
+      &-head {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        gap: 30px;
+        justify-items: center;
+        grid-template-areas:
+        "ngocmai ngocmai ngoctai ngoctai duydat duydat"
+        "congdi congdi congdi quoccuong quoccuong quoccuong";
+
+        > div[class$=item] {
+          // Đội Trưởng
+          &:nth-child(1) {
+            grid-area: ngoctai;
+            @media (max-width: 559px) {
+             justify-self: center;
+            }
+          }
+          &:nth-child(2) {
+            grid-area: ngocmai;
+            justify-self: end;
+          }
+          &:nth-child(3) {
+            grid-area: duydat;
+            justify-self: start;
+          }
+
+          &:nth-last-child(1) {
+            grid-area: quoccuong;
+            justify-self: start;
+          }
+          &:nth-last-child(2) {
+            grid-area: congdi;
+            justify-self: end;
+          }
+        }
+
+        @media (max-width: 650px) {
+          grid-template-areas:
+          "ngoctai ngoctai ngoctai ngoctai ngoctai ngoctai"
+          "ngocmai ngocmai ngocmai duydat duydat duydat"
+          "congdi congdi congdi quoccuong quoccuong quoccuong";
+        }
+        @media (max-width: 430px) {
+          grid-template-areas:
+          "ngoctai ngoctai ngoctai ngoctai ngoctai ngoctai"
+          "ngocmai ngocmai ngocmai ngocmai ngocmai ngocmai"
+          "duydat duydat duydat duydat duydat duydat"
+          "congdi congdi congdi congdi congdi congdi"
+          "quoccuong quoccuong quoccuong quoccuong quoccuong quoccuong";
+        }
+      }
+
+      &-others {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+        gap: 30px;
+        justify-items: center;
+
+        padding-top: 35px;
+        border-top: 1px solid rgba(0, 0, 0, 0.05);
+      }
     }
+  }
+
+  &-stars {
+
   }
 }
 </style>
