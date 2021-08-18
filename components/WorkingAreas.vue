59ff8027b981434466e7a7e89c207b23013222e4
iceStorm <hauduadaiduong@gmail.com>
2021-08-19 01:44:57 +0700

    fix working-areas item shadow

--- components/WorkingAreas.vue
+++ components/WorkingAreas.vue
@@ -153,6 +153,7 @@ export default {
 
       border-radius: 0px;
       box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+      box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 
       &::after {
         content: "";
